65a592f516b437ad10135a439ac52db7ac0287ad
santiagoalonso-ui <santiago.alonso@uniat.edu.mx>
2025-12-01 00:53:46 -0600

    Main Menu Start
    Primera fase del main menu

---
+++ VoxelesSurvival/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+
+
